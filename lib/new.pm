82caff0528d088f46bec7c09f3da3f792d755455
Matt S Trout <mst@shadowcat.co.uk>
2020-03-28 16:04:06 +0000

    initial commit from distar-init

---
+++ lib/new.pm
@@ -0,0 +1,33 @@
+package new;
+
+our $VERSION = '0.000001'; # 0.0.1
+
+$VERSION = eval $VERSION;
+
+1;
+
+=head1 NAME
+
+new - Description goes here
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+ mst - Matt S. Trout (cpan:MSTROUT) <mst@shadowcat.co.uk>
+
+=head1 CONTRIBUTORS
+
+None yet - maybe this software is perfect! (ahahahahahahahahaha)
+
+=head1 COPYRIGHT
+
+Copyright (c) 2020 the new L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
