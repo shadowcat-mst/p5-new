6f5434aba78962da1869a766bfa15988f92b6a58
Matt S Trout <mst@shadowcat.co.uk>
2020-03-28 18:29:46 +0000

    podulate

--- lib/new.pm
+++ lib/new.pm
@@ -22,11 +22,48 @@ sub import {
 
 =head1 NAME
 
-new - Description goes here
+new - Object instantiation sugar for one-liners
 
 =head1 SYNOPSIS
 
+Simplest possible usage:
+
+  perl -Mnew=HTTP::Tiny -E \
+    'say $O->get("http://trout.me.uk/X11/vimrc")->{content}'
+
+With arguments:
+
+  perl -Mnew=HTTP::Tiny,max_redirects,3 -E \
+    'say $O->get("http://trout.me.uk/X11/vimrc")->{content}'
+
+With custom object name:
+
+  perl -Mnew=HTTP::Tiny,\$H -E \
+    'say $H->get("http://trout.me.uk/X11/vimrc")->{content}'
+
+With both:
+
+  perl -Mnew=HTTP::Tiny,\$H,max_redirects,3 -E \
+    'say $H->get("http://trout.me.uk/X11/vimrc")->{content}'
+
 =head1 DESCRIPTION
+
+=head2 import
+
+  new->import($class, @args)
+
+First we C<require> the file for C<$class>, then call
+
+  $class->new(@args)
+
+then install the resulting object in C<$O> in the calling package.
+
+If the first argument to C<import> after C<$class> begins with C<$>, this
+is treated as the name to install the object as, so
+
+  new->import($class, '$Obj', @args);
+
+will create a variable C<$Obj> in the calling package instead of C<$O>.
 
 =head1 AUTHOR
 
